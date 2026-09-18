2ec6bdb60ac45a642a493b7766e20a3c7977eedc
Ian-MICTO <ianmicto@gmail.com>
2026-09-18 13:47:49 +0800

    fix: update frontend docker build args for render backend

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -14,11 +14,13 @@ export default defineConfig({
       STRAPI_URL: envField.string({
         context: "server",
         access: "public",
-        default: "http://localhost:1337",
+        default: "https://aieien-backend.onrender.com",
       }),
       STRAPI_API_TOKEN: envField.string({
         context: "server",
         access: "secret",
+        default:
+          "e9a529e01acdf94252c971ed2eace29b1bca402851e7a072f898d6214082edb9d18c5f925839f0b5815ed2dab529667ccbe04f4bac38b229c715d42d6b263cc9084431ce2a2f2a6ef75ee269131c9b4051d59091814164f242b242af1fe4545d5c5161171446b4451e5698f68629f9335c6ad58282de3b7ab103280bc5c47f98",
         optional: true,
       }),
     },
